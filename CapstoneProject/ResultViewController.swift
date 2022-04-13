6a5a707fe6a58df67faaf573026d551a3caa1d13
ZhibinYU <1355958493@qq.com>
2022-04-13 16:46:54 -0400

    16:46

---
+++ CapstoneProject/ResultViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ResultViewController.swift
+//  CapstoneProject
+//
+//  Created by 志斌Zhibin 于Yu on 2022-04-13.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func homePageButtom(_ sender: UIButton) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
